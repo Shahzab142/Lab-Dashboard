1d31fd8a1b0ef70b98a3aea8af493e5c830395ae
Shahzaib Munawar <shahzaibmunawar920@gmail.com>
2026-02-15 19:11:29 +0500

    Fix syntax in supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,5 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-"https://wsbtgnfembwffsshughi.supabase.co",
+export const supabase = createClient(
+  "https://wsbtgnfembwffsshughi.supabase.co",
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndzYnRnbmZlbWJ3ZmZzc2h1Z2hpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg2Mzc2MjMsImV4cCI6MjA4NDIxMzYyM30.49iG3vK6VcJg6676L6cx7055PP4UFSWfY9gcgJLp5zs"
 );
